8df436e934cdaf89cbcb8fb171db31e9e2e2ba9f
cthors <57840413+cthors@users.noreply.github.com>
2026-03-21 16:28:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -64,8 +64,7 @@ function notificationDemo(){
 
 // Add a class for the type of page we're on, to apply page-specific styles:
 let page = window.location.pathname.split('/').pop();
-console.log("PAGE: " + page);
-if(page=="index.html"){
+if(page=="index.html" || page==""){
 	document.querySelector('body').classList.add('front-page');
 }
 else{
